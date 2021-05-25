d05fbcdbf43d20b05416b2f433f8c65a3d15118b
Bishwo Maharjan <bsomhz@hotmail.com>
2021-05-25 10:31:03 +0545

    testcase created

--- dojo/admin.py
+++ dojo/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from . models import Dojo, Operator, Player
+from .models import Dojo, Operator, Player
+
 
 # Register your models here.
-admin.site.register(Dojo)
-admin.site.register(Operator)
-admin.site.register(Player)
+@admin.register(Dojo)
+class DojoAdmin(admin.ModelAdmin):
+    list_display = ['name', 'province', 'state']
+
+
+@admin.register(Operator)
+class OperatorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name',  'province', 'state']
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name',  'weight', 'height']
--- dojo/tests.py
+++ dojo/tests.py
@@ -1,3 +1,17 @@
+from dojo.models import Operator
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class UnitTestCase(TestCase):
+
+    def test_home_page_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'dojo/dojo.html')
+
+    def test_operator(self):
+        operator = Operator()
+        operator.first_name = 'Bikash'
+        operator.last_name = 'Maharjan'
+        operator.save()
